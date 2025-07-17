01e8ce04b18fb01e876799b48a1a03cb723f102f
ck-code-98 <constantin-knappe@web.de>
2025-07-17 16:13:23 +0200

    corrected the speed converter

--- speed_converter.py
+++ speed_converter.py
@@ -1,3 +1,5 @@
 def mps_to_kph(speed_in_mps):
-    speed_in_kph = speed_in_mps * 5793.64
+    """ Convert speed from meters per second to kilometers per hour.
+    Parameter: mps (float) --- Return: kph (float)"""
+    speed_in_kph = speed_in_mps * 3.6
     return speed_in_kph
